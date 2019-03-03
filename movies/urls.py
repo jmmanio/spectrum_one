89d3b532de5ddc0a7f45ecaeb5d7b85c4f7d5b53
jmmanio <josemari.manio@gmail.com>
2019-03-04 00:22:56 +0800

    Updated app 'movies' urls.py

--- movies/urls.py
+++ movies/urls.py
@@ -7,8 +7,8 @@ app = 'movies'
 urlpatterns = [
     path('checktitle', Movies.check_title, name='checktitle'),
     path('add', Movies.add, name='add'),
-    url(r'^details.+/?$', Movies.details, name='details'),
-    url(r'^edit.+/?$', Movies.edit, name='edit'),
+    url(r'^details[.]*/?$', Movies.details, name='details'),
+    url(r'^edit[.]*/?$', Movies.edit, name='edit'),
     path('delete', Movies.delete, name='delete'),
     path('like', Movies.like, name='like'),
 ]
